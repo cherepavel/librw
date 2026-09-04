ab7105eff5a73eed57f54b3b409e808387898d92
Pavel Frasyn <frasyn.pashka@gmail.com>
2026-09-04 09:15:55 +0300

    Convert D3D8 BGRA8888 textures on PSP

--- src/psp/pspnative.cpp
+++ src/psp/pspnative.cpp
@@ -1,6 +1,7 @@
 #ifdef RW_PSP
 
 #include <stdio.h>
+#include <string.h>
 
 #include "../rwbase.h"
 #include "../rwerror.h"
@@ -36,27 +37,35 @@ readNativeTexture(Stream *stream)
 	stream->read8(texture->mask, 32);
 
 	int32 format = stream->readI32();
-	stream->readI32(); // hasAlpha; the palette contains authoritative alpha
+	stream->readI32(); // hasAlpha; source pixels/palette contain authoritative alpha
 	int32 width = stream->readU16();
 	int32 height = stream->readU16();
 	int32 depth = stream->readU8();
 	int32 numLevels = stream->readU8();
 	int32 type = stream->readU8();
 	int32 compression = stream->readU8();
-	if(width <= 0 || height <= 0 || numLevels <= 0 || depth != 8 ||
-	   type != Raster::TEXTURE || compression != 0 ||
-	   (format & (Raster::PAL4 | Raster::PAL8)) != Raster::PAL8){
+	bool32 pal8 = depth == 8 &&
+		(format & (Raster::PAL4 | Raster::PAL8)) == Raster::PAL8;
+	bool32 bgra8888 = depth == 32 && (format & 0xF00) == Raster::C8888 &&
+		(format & (Raster::PAL4 | Raster::PAL8)) == 0;
+	if(width <= 0 || height <= 0 || numLevels <= 0 ||
+	   type != Raster::TEXTURE || compression != 0 || (!pal8 && !bgra8888)){
 		texture->destroy();
 		return nil;
 	}
 
-	format &= ~Raster::PAL4;
-	format |= Raster::PAL8;
-	if(numLevels > 1)
-		format |= Raster::MIPMAP;
+	int32 destinationFormat;
+	if(pal8)
+		destinationFormat = (format & ~Raster::PAL4) | Raster::PAL8;
 	else
-		format &= ~Raster::MIPMAP;
-	Raster *raster = Raster::create(width, height, depth, format | type,
+		destinationFormat = (format & ~(0xF00 | Raster::PAL4 | Raster::PAL8)) |
+			Raster::C4444;
+	if(numLevels > 1)
+		destinationFormat |= Raster::MIPMAP;
+	else
+		destinationFormat &= ~Raster::MIPMAP;
+	Raster *raster = Raster::create(width, height, pal8 ? 8 : 16,
+		destinationFormat | type,
 		PLATFORM_PSP);
 	if(raster == nil){
 		texture->destroy();
@@ -64,13 +73,15 @@ readNativeTexture(Stream *stream)
 	}
 	texture->raster = raster;
 
-	uint8 *palette = raster->lockPalette(Raster::LOCKWRITE | Raster::LOCKNOFETCH);
-	if(palette == nil || stream->read8(palette, 256*4) != 256*4){
-		if(palette) raster->unlockPalette();
-		texture->destroy();
-		return nil;
+	if(pal8){
+		uint8 *palette = raster->lockPalette(Raster::LOCKWRITE | Raster::LOCKNOFETCH);
+		if(palette == nil || stream->read8(palette, 256*4) != 256*4){
+			if(palette) raster->unlockPalette();
+			texture->destroy();
+			return nil;
+		}
+		raster->unlockPalette();
 	}
-	raster->unlockPalette();
 
 	for(int32 level = 0; level < numLevels; level++){
 		uint32 size = stream->readU32();
@@ -78,15 +89,38 @@ readNativeTexture(Stream *stream)
 			stream->seek(size);
 			continue;
 		}
-		uint8 *pixels = raster->lock(level,
-			Raster::LOCKWRITE | Raster::LOCKNOFETCH);
-		uint32 expected = pixels ? raster->stride*raster->height : 0;
-		if(pixels == nil || size != expected || stream->read8(pixels, size) != size){
-			if(pixels) raster->unlock(level);
-			texture->destroy();
-			return nil;
+		if(pal8){
+			uint8 *pixels = raster->lock(level,
+				Raster::LOCKWRITE | Raster::LOCKNOFETCH);
+			uint32 expected = pixels ? raster->stride*raster->height : 0;
+			if(pixels == nil || size != expected || stream->read8(pixels, size) != size){
+				if(pixels) raster->unlock(level);
+				texture->destroy();
+				return nil;
+			}
+			raster->unlock(level);
+		}else{
+			uint32 sourceSize = raster->width*raster->height*4;
+			uint8 *source = (uint8*)rwMalloc(sourceSize, MEMDUR_FUNCTION | ID_RASTERPSP);
+			uint8 *pixels = raster->lock(level,
+				Raster::LOCKWRITE | Raster::LOCKNOFETCH);
+			if(source == nil || pixels == nil || size != sourceSize ||
+			   stream->read8(source, sourceSize) != sourceSize){
+				if(pixels) raster->unlock(level);
+				if(source) rwFree(source);
+				texture->destroy();
+				return nil;
+			}
+			for(int32 y = 0; y < raster->height; y++)
+				for(int32 x = 0; x < raster->width; x++){
+					uint8 *bgra = source + (y*raster->width + x)*4;
+					uint16 rgba = (uint16)((bgra[2] >> 4) | ((bgra[1] >> 4) << 4) |
+						((bgra[0] >> 4) << 8) | ((bgra[3] >> 4) << 12));
+					memcpy(pixels + y*raster->stride + x*2, &rgba, 2);
+				}
+			raster->unlock(level);
+			rwFree(source);
 		}
-		raster->unlock(level);
 	}
 	return texture;
 }
